4357261f749109575529fb245598f15f4cff99ae
Artem Rakhmatulin <rakhmatulin@gmail.com>
2021-07-05 10:42:02 +0200

    Initial Commit

---
+++ CoreDataVsRealm/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CoreDataVsRealm
+//
+//  Created by Artem Rakhmatulin on 05.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ CoreDataVsRealm/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  CoreDataVsRealm
+//
+//  Created by Artem Rakhmatulin on 05.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
